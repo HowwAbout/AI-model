df01b90605e9f0794f01d1de0b5052c9be46a75b
jwywoo <jwywoo26@gmail.com>
2024-09-09 17:29:07 +0900

    [FEAT] #5-2 RAG API Crud
    - Basic Connection Checked

---
+++ app/crud/dating_crud.py
@@ -0,0 +1,6 @@
+from ..schema.response_dto.dating_generation_response_dto import DatingGenResponseDto
+from ..schema.request_dto.dating_generation_request_dto import DatingGenRequestDto
+
+
+def dating_generation(request: DatingGenRequestDto):
+    return "Connected"
