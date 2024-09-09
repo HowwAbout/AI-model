1c70dcf9fb107f50742c6dba7e7e4ca89b96e1c9
jwywoo <jwywoo26@gmail.com>
2024-09-09 17:28:12 +0900

    [FEAT] #5-1 RAG Api Router
    - main.py
    - dating_generation_router.py(ai/api/dating/generate)

--- app/main.py
+++ app/main.py
@@ -2,11 +2,16 @@ from dotenv import load_dotenv
 
 from fastapi import FastAPI, Request
 
+from .routers import dating_generation_router
+
 # Config
 load_dotenv()
 
 # Server Init
 app = FastAPI()
+
+# Router
+app.include_router(dating_generation_router.router)
 
 @app.get("/")
 def root():
---
+++ app/routers/dating_generation_router.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+
+from ..schema.request_dto.dating_generation_request_dto import DatingGenRequestDto
+from ..schema.response_dto.dating_generation_response_dto import DatingGenResponseDto
+
+from ..crud.dating_crud import dating_generation
+
+router = APIRouter()
+
+
+@router.post("ai/dating/generate", response_model=list[DatingGenResponseDto])
+def dating_generation_router(request: DatingGenRequestDto) -> list[DatingGenRequestDto]:
+    return dating_generation(request)
